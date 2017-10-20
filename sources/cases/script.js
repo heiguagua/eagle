c1d9c41b677a7c029031d5c44331dd2d6ce30db4
hank.zheng <zhengh@chinawiserv.com>
2017-10-20 17:15:07 +0800

    Update

--- sources/cases/script.js
+++ sources/cases/script.js
@@ -1,6 +1,42 @@
+import Http from '../common/scripts/http.js';
+import Encrypt from '../common/scripts/encrypt.js';
 export default {
   data() {
     return {}
   },
-  methods: {}
+  methods: {
+    get() {
+      const vm = this;
+      Http.fetch({
+          method: 'GET',
+          url: Http.url.master + '/legal_case/list',
+          data: {
+            loginName: vm.loginName,
+            password: Encrypt.md5(vm.password)
+          }
+        })
+        .then(function (result) {
+          const data = result.data;
+          if (Http.protocol(data, 200)) {
+            return data
+          }
+        })
+        .then(function (data) {
+          vm.$message({
+            message: data.head.message
+          });
+          return data;
+        })
+        .then(function (data) {
+          Encrypt.token.set(data.head.token);
+          return data;
+        })
+        .then(function (data) {
+          vm.$router.push('/layout/cases');
+        });
+    }
+  },
+  mounted: {
+
+  }
 };
