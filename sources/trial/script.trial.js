44bdea39953a69818b18bc538af8f78c0c192649
hank.zheng <zhengh@chinawiserv.com>
2017-11-01 18:06:12 +0800

    Update

---
+++ sources/trial/script.trial.js
@@ -0,0 +1,63 @@
+export default {
+  // 庭审信息
+  infomation: '',
+  // 法庭纪律
+  discipline: '',
+  // 信息核对
+  verification: {
+    // 诉讼参与人
+    participator: '',
+    // 异议及理由
+    objection: '',
+    // 是否有证人出庭
+    witness: '',
+  },
+  // 庭审序言
+  preface: {
+    // 庭审组织
+    organize: '',
+    // 告知诉讼权利
+    rights: '',
+    // 告知事项
+    matter: '',
+    // 是否申请回避
+    evasion: '',
+    // 举证期限异议
+    evidence: ''
+  },
+  // 法庭调查
+  investigate: {
+    // 诉辩意见
+    opinion: '',
+    // 法庭询问
+    inquiry: '',
+    // 举证质证
+    proof: '',
+    // 争点归纳
+    conclude: {
+      // 无争议归纳
+      undisputed: '',
+      // 争议归纳
+      disputed: ''
+    }
+  },
+  // 法庭辩论
+  argument: {
+    // 原告方辩论意见
+    accuser: '',
+    // 被告方辩论意见
+    accused: '',
+    // 第三人辩论意见
+    thirdparty: '',
+    // 庭审总结
+    summary: '',
+  },
+  // 最后陈述
+  statement: '',
+  // 法庭调解
+  conciliation: '',
+  // 休庭宣读
+  announce: '',
+  // 其他
+  other: ''
+}
