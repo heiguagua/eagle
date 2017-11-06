2f83b079467872bd124b64fbd8a5628f45b93dd2
hank.zheng <zhengh@chinawiserv.com>
2017-11-06 20:08:23 +0800

    Update

--- sources/store.js
+++ sources/store.js
@@ -1,28 +1,28 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import TrialArticleToolbar from './trial/article/toolbar/script.store';
+// import Vue from 'vue';
+// import Vuex from 'vuex';
+// import TrialArticleToolbar from './trial/article/toolbar/script.store';
 
-Vue.use(Vuex);
+// Vue.use(Vuex);
 
-const Store = new Vuex.Store({
-  strict: process.env.NODE_ENV !== 'production',
-  modules: {
-    TrialArticleToolbar
-  }
-});
+// const Store = new Vuex.Store({
+//   strict: process.env.NODE_ENV !== 'production',
+//   modules: {
+//     TrialArticleToolbar
+//   }
+// });
 
-if (module.hot) {
-  module.hot.accept([
-    './store',
-    './trial/article/toolbar/script.store',
-  ], () => {
-    console.info('Vue hot update!');
-    Store.hotUpdate({
-      modules: {
-        TrialArticleToolbar: require('./trial/article/toolbar/script.store').default
-      }
-    })
-  })
-};
+// if (module.hot) {
+//   module.hot.accept([
+//     './store',
+//     './trial/article/toolbar/script.store',
+//   ], () => {
+//     console.info('Vue hot update!');
+//     Store.hotUpdate({
+//       modules: {
+//         TrialArticleToolbar: require('./trial/article/toolbar/script.store').default
+//       }
+//     })
+//   })
+// };
 
-export default Store;
+// export default Store;
--- sources/trial/article/script.js
+++ sources/trial/article/script.js
@@ -1,13 +1,11 @@
 import Http from '../../common/scripts/http';
 import ArticleBlank from './blank';
 import ArticleEditor from './editor';
-import ArticleToolbar from './toolbar';
 import { mapState, mapActions, mapMutations } from 'vuex';
 
 export default {
   components: {
     ArticleBlank,
-    ArticleToolbar,
     ArticleEditor
   },
   data() {
@@ -21,12 +19,6 @@ export default {
       this.$router.push('/layout/trial/blank')
     },
   },
-  computed: {
-    ...mapState('TrialArticleToolbar', {
-      currentView: state => state.currentView
-    }),
-  },
-  mounted() {
-
-  }
+  computed: {},
+  mounted() {}
 };
