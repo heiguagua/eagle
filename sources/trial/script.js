13401338492423c5e19476166571b8eb6997d4e2
hank.zheng <zhengh@chinawiserv.com>
2017-11-08 18:11:21 +0800

    Update

--- sources/trial/script.js
+++ sources/trial/script.js
@@ -1,10 +1,8 @@
-import TrialArticle from './article';
 import TrialSidebar from './sidebar';
 import TrialNavbar from './navbar';
 
 export default {
   components: {
-    TrialArticle,
     TrialSidebar,
     TrialNavbar
   },
