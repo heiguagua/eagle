3ef0af15ceaf94d1d8f87226380ae8a07ee5f593
hank.zheng <zhengh@chinawiserv.com>
2017-11-09 19:58:12 +0800

    Update

--- sources/trial/article/editor/script.vue.computed.js
+++ sources/trial/article/editor/script.vue.computed.js
@@ -1,3 +1,9 @@
 export default {
-
+  isChiefOfficer() {
+    if (this.trial.infomation.officer[0].duty !== '审判长') {
+      this.trial.infomation.officer[1] = {};
+      this.trial.infomation.officer[2] = {};
+      return true;
+    }
+  }
 };
