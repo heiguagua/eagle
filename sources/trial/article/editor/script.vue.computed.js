4c4646ea84595c39aba950023ea42b5040e4a374
hank.zheng <zhengh@chinawiserv.com>
2017-11-09 20:30:08 +0800

    Update

--- sources/trial/article/editor/script.vue.computed.js
+++ sources/trial/article/editor/script.vue.computed.js
@@ -1,9 +1,11 @@
 export default {
   isChiefOfficer() {
-    if (this.trial.infomation.officer[0].duty !== '审判长') {
-      this.trial.infomation.officer[1] = {};
-      this.trial.infomation.officer[2] = {};
+    if (this.trial.infomation.officer[0].duty === '审判长') {
       return true;
+    } else {
+      this.trial.infomation.officer[1].duty = '';
+      this.trial.infomation.officer[2].duty = '';
+      return false;
     }
   }
 };
