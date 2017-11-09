7b05851ad16e4c424393dceac3491a462887ed9a
hank.zheng <zhengh@chinawiserv.com>
2017-11-09 20:37:58 +0800

    Update

---
+++ sources/trial/article/preview/script.js
@@ -0,0 +1,13 @@
+import Http from '../../../common/scripts/http';
+import { message } from '../../../common/scripts/helper';
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  render() {
+    return <h1>JSX</h1>
+  }
+};
