c6db8be5389b45c4e701e7778a1b0d5cd43487d4
hank.zheng <zhengh@chinawiserv.com>
2017-11-14 16:06:59 +0800

    Update

--- sources/trial/article/editor/script.vue.computed.js
+++ sources/trial/article/editor/script.vue.computed.js
@@ -3,8 +3,6 @@ export default {
     if (this.trial.infomation.officer[0].duty === '审判长') {
       return true;
     } else {
-      this.trial.infomation.officer[1].duty = '';
-      this.trial.infomation.officer[2].duty = '';
       return false;
     }
   }
