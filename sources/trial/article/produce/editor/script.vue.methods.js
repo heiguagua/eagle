2c0d4151f866b4d100bf684ef4e2363387b5522e
hank.zheng <uinika@outlook.com>
2017-11-17 01:53:45 +0800

    Update

--- sources/trial/article/produce/editor/script.vue.methods.js
+++ sources/trial/article/produce/editor/script.vue.methods.js
@@ -3,21 +3,30 @@ import { message } from '../../../../common/scripts/helper';
 
 export default {
   /* 增加原告 */
-  pushAccuser() {
+  addAccuser() {
     const vm = this;
     const accuser = Trial().verification.participator.accusers[0];
     this.trial.verification.participator.accusers.push(accuser);
     message(vm, 'success', '温馨提示：诉讼参予人-原告添加成功！');
   },
+  /* 删除原告 */
+  removeAccuser(index) {
+    const accusers = this.trial.verification.participator.accusers;
+    accusers.splice(index, 1);
+  },
+  /*  */
+  addAccuserSubject() {
+
+  },
   /* 增加被告 */
-  pushAccused() {
+  addAccused() {
     const vm = this;
     const accused = Trial().verification.participator.accuseds[0];
     this.trial.verification.participator.accuseds.push(accused);
     message(vm, 'success', '温馨提示：诉讼参予人-被告添加成功！');
   },
   /* 增加第三人 */
-  pushThirdparty() {
+  addThirdparty() {
     const vm = this;
     const thirdparty = Trial().verification.participator.thirdparties[0];
     this.trial.verification.participator.thirdparties.push(thirdparty);
