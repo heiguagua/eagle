0e6a6687b2edad9a0d8bf843d04f5de6d0566c77
hank.zheng <zhengh@chinawiserv.com>
2017-11-21 15:31:58 +0800

    Update

--- sources/trial/article/preview/script.js
+++ sources/trial/article/preview/script.js
@@ -1,6 +1,6 @@
 import Http from "../../../common/scripts/http";
-import { message } from "../../../common/scripts/helper";
 import JudgeTemplate from "./template";
+import { message, storage } from "../../../common/scripts/helper";
 
 export default {
   components: {
@@ -12,6 +12,29 @@ export default {
     };
   },
   methods: {
+    save() {
+      const vm = this;
+      const code = storage.get("case").case_no; // 案号
+      const json = JSON.stringify(storage.get("trial")); // 庭审笔录状态树
+      const html = vm.$el.innerHTML.replace(/\sdata-v-\w*=""/g, "");
+      Http.fetch({
+          method: "POST",
+          url: Http.url.master + "/trial",
+          data: {
+            case_no: code,
+            json: json,
+            html: html,
+          }
+        })
+        .then(result => {
+          const data = result.data;
+          if (Http.protocol(data, 200)) {
+            message(vm, "success", data.head.message);
+          } else {
+            message(vm, "warning", data.head.message);
+          }
+        });
+    },
     toWord() {
       Http.fetch({
           method: "POST",
--- sources/trial/article/preview/template/script.js
+++ sources/trial/article/preview/template/script.js
@@ -1,13 +1,20 @@
 import Http from "../../../../common/scripts/http";
-import { storage } from "../../../../common/scripts/helper";
+import { message, storage } from "../../../../common/scripts/helper";
 
 export default {
   data() {
     return {
+      case: {},
       trial: {}
     };
   },
   created() {
+    this.case = storage.get("case");
     this.trial = storage.get("trial");
+  },
+  methods: {
+    save() {
+
+    }
   }
 };
