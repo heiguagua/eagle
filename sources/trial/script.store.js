cd3595dae3bb63c37dd6c82f484281b47fa04686
hank.zheng <zhengh@chinawiserv.com>
2017-11-21 17:33:56 +0800

    Update

--- sources/trial/script.store.js
+++ sources/trial/script.store.js
@@ -1,17 +1,47 @@
+import Http from "../common/scripts/http";
+import { message, storage } from "../common/scripts/helper";
+
 export default {
   namespaced: true,
   state: {
     trial: {},
-    trialList: [],
+    trials: [],
+    material: {},
+    materials: [],
   },
   mutations: {
-    syncTrial(state, payload) {
+    setTrial(state, payload) {
+      state.trial = payload;
+    },
+    setTrials(state, payload) {
       state.trial = payload;
     }
   },
   actions: {
-    action(context, payload) {
-      context.commit("syncTrial", payload)
+    getTrial(context, payload) {
+      (params) => {
+        return Http.fetch({
+          method: "GET",
+          url: Http.url.master + "/legal_case/list",
+          params: params
+        })
+      }
+    },
+    getTrials(context, payload) {
+      Http.fetch({
+          method: "GET",
+          url: Http.url.master + "/trials",
+          params: params
+        })
+        .then(function(result) {
+          const data = result.data;
+          if (Http.protocol(data, 200)) {
+            message(vm, "info", data.head.message);
+            context.commit("setTrials", data);
+          } else {
+            message(vm, "warning", data.head.message);
+          }
+        })
     }
   },
   getters: {}
