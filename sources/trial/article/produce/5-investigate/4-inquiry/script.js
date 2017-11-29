0f4c4c8d930526c6f3d3239b1dc074c86a9332ed
hank.zheng <zhengh@chinawiserv.com>
2017-11-29 15:48:21 +0800

    Update

---
+++ sources/trial/article/produce/5-investigate/4-inquiry/script.js
@@ -0,0 +1,14 @@
+import { mapMutations, mapActions, mapState, mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState("Trial", [
+      "trial",
+      "options"
+    ]),
+  },
+  methods: {},
+};
