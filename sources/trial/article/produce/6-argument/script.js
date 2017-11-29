419b496748f7cd40087c414c390ed4e392591da3
hank.zheng <zhengh@chinawiserv.com>
2017-11-29 15:36:33 +0800

    Update

--- sources/trial/article/produce/6-argument/script.js
+++ sources/trial/article/produce/6-argument/script.js
@@ -1,6 +1,17 @@
 import { mapMutations, mapActions, mapState, mapGetters } from "vuex";
+import ArgumentAccuser from "./accuser";
+import ArgumentAccused from "./accused";
+import ArgumentSummary from "./summary";
+import ArgumentThirdparty from "./thirdparty";
+
 
 export default {
+  components: {
+    ArgumentAccuser,
+    ArgumentAccused,
+    ArgumentSummary,
+    ArgumentThirdparty,
+  },
   data() {
     return {}
   },
