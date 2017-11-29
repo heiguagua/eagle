2c2ed7cbf972796e3e19bc8e98c44a41c7b70ad0
hank.zheng <zhengh@chinawiserv.com>
2017-11-29 15:57:26 +0800

    Update

--- sources/trial/article/produce/5-investigate/script.js
+++ sources/trial/article/produce/5-investigate/script.js
@@ -1,6 +1,20 @@
 import { mapMutations, mapActions, mapState, mapGetters } from "vuex";
+import InvestigateOpinion from "./1-opinion";
+import InvestigateUndisputed from "./2-undisputed";
+import InvestigateDisputed from "./3-disputed";
+import InvestigateInquiry from "./4-inquiry";
+import InvestigateProof from "./5-proof";
+import InvestigateFact from "./6-fact";
 
 export default {
+  components: {
+    InvestigateOpinion,
+    InvestigateUndisputed,
+    InvestigateDisputed,
+    InvestigateInquiry,
+    InvestigateProof,
+    InvestigateFact
+  },
   data() {
     return {}
   },
