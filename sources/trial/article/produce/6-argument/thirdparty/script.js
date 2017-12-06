758e866d4e09a7f4e405b205a3bdcd40cd1e4721
denghui <dengh@chinawiserv.com>
2017-12-06 16:22:55 +0800

    modify

--- sources/trial/article/produce/6-argument/thirdparty/script.js
+++ sources/trial/article/produce/6-argument/thirdparty/script.js
@@ -1,10 +1,11 @@
 import { mapMutations, mapActions, mapState, mapGetters } from "vuex";
 
 class DataBase { 
-  constructor(participator,item,flag){
+  constructor(participator, flag, index){
     this.participator = participator;
-    flag === true ? (this.item = item - 1) : (this.item = item + 1)
+    // flag === true ? (this.item = item - 1) : (this.item = item + 1)
     this.flag = flag; //表示是添加还是删除(false ==> 删除  true ==> 添加)
+    this.index = index;
     this.getData()
     this.updataItemData()
   }
@@ -13,21 +14,21 @@ class DataBase {
     this.accusers = this.participator.accusers
     this.thirdparties = this.participator.thirdparties
   }
-  updataItemData(){
-    this.updata(this.accuseds)
-    this.updata(this.accusers)
-    this.updata(this.thirdparties)
+  updataItemData(index){
+    this.updata(this.accuseds,index)
+    this.updata(this.accusers,index)
+    this.updata(this.thirdparties,index)
   }
-  updata(data,key){
+  updata(data,key,index){
     data.forEach((item) => {
       if(this.flag){
         this.addChildren(item.subjects)
         this.addChildren(item.responsibles)
         this.addChildren(item.agents)
       }else{
-        this.removeChildren(item.subjects)
-        this.removeChildren(item.responsibles)
-        this.removeChildren(item.agents)
+        this.removeChildren(item.subjects,index)
+        this.removeChildren(item.responsibles,index)
+        this.removeChildren(item.agents,index)
       }
     })
   }
