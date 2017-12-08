7c61a6bd79eae51c51c5fc3e1bc7141c469ecc32
hank.zheng <zhengh@chinawiserv.com>
2017-12-08 15:10:32 +0800

    Update

--- sources/trial/article/produce/3-verification/accused/script.js
+++ sources/trial/article/produce/3-verification/accused/script.js
@@ -29,165 +29,143 @@ export default {
       }
       return array
     },
-    toCourtChange(obj) {//到庭，未到庭的选择事件
-      obj.isAppear == "到庭" ? (obj.showFlag = true) : (obj.showFlag = false);
-      Util.showSetting(this.trial);
-      // vm.event.verify.verify_participant.getAbsentee(); //存储未到庭人员 （诉讼地位名：姓名）
-      // vm.event.verify.verify_participant.lagalAgentChange(obj);
-      // vm.event.verify.verify_participant.getToCourt(); //存储到庭人员 （诉讼地位名：姓名）
-    },
-    accusedHandler(target, operation, params) {
+    accusedEvent(operation, params) {
       const vm = this;
-      switch (target) {
-        case "accused":
-          {
-            // 添加
-            if (operation === "add") {
-              let accused = Trial().verification.participator.accuseds[0];
-              accused.subjects[0]["ordinal"] = Util.addNum('subjects', 'accuseds', this.trial).length + 1;
-              // 法庭询问,添加了几个问题
-              let len = this.trial.investigate.inquiry.elementquerys.length || 0;
-              if (len) {
-                for (let j = 0; j < accused.subjects.length; j++) {
-                  accused.subjects[j].inquiry = this.getArray(len);
-                }
-                for (let j = 0; j < accused.responsibles.length; j++) {
-                  accused.responsibles[j].inquiry = this.getArray(len);
-                }
-                for (let j = 0; j < accused.subjects.length; j++) {
-                  accused.agents[j].inquiry = this.getArray(len);
-                }
-              }
-              // 法庭辩论，添加几轮辩论
-              let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
-              if (debateArrayLen) {
-                for (let j = 0; j < accused.subjects.length; j++) {
-                  accused.subjects[j].argument = this.getArgumentArray(debateArrayLen);
-                }
-                for (let j = 0; j < accused.responsibles.length; j++) {
-                  accused.responsibles[j].argument = this.getArgumentArray(debateArrayLen);
-                }
-                for (let j = 0; j < accused.subjects.length; j++) {
-                  accused.agents[j].argument = this.getArgumentArray(debateArrayLen);
-                }
-              }
-              this.trial.verification.participator.accuseds.push(accused);
-              message(vm, "success", "温馨提示：被告添加成功！");
-            }
-            // 删除
-            else if (operation === "remove") {
-              let accusedIndex = params.accusedIndex;
-              if (accusedIndex !== 0) {
-                this.trial.verification.participator.accuseds.splice(accusedIndex, 1);
-                Util.updateNum('subjects', 'accuseds', this.trial);
-                message(vm, "warning", "温馨提示：被告删除成功！");
-              } else {
-                message(vm, "error", "温馨提示：不能删除唯一的被告！");
-              }
-            }
+      /* 添加操作 */
+      if (operation === "add") {
+        let accused = Trial().verification.participator.accuseds[0];
+        accused.subjects[0]["ordinal"] = Util.addNum('subjects', 'accuseds', this.trial).length + 1;
+        // 法庭询问,添加了几个问题
+        let len = this.trial.investigate.inquiry.elementquerys.length || 0;
+        if (len) {
+          for (let j = 0; j < accused.subjects.length; j++) {
+            accused.subjects[j].inquiry = this.getArray(len);
           }
-          break;
-        case "subject":
-          {
-            // 添加
-            if (operation === "add") {
-              let accusedIndex = params.accusedIndex;
-              let originSubject = Trial().verification.participator.accuseds[0].subjects[0];
-              originSubject["ordinal"] = Util.addNum('subjects', 'accuseds', this.trial).length + 1;
-              // 法庭询问,添加了几个问题
-              let len = this.trial.investigate.inquiry.elementquerys.length || 0;
-              if (len) {
-                originSubject.inquiry = this.getArray(len);
-              }
-              // 法庭辩论
-              let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
-              if (debateArrayLen) {
-                originSubject.argument = this.getArgumentArray(debateArrayLen);
-              }
-              let targetSubject = this.trial.verification.participator.accuseds[accusedIndex].subjects.push(originSubject);
-              message(vm, "success", "温馨提示：被告诉讼主体添加成功！");
-            }
-            // 删除
-            else if (operation === "remove") {
-              let accusedIndex = params.accusedIndex;
-              let subjectIndex = params.subjectIndex;
-              if (subjectIndex !== 0) {
-                this.trial.verification.participator.accuseds[accusedIndex].subjects.splice(subjectIndex, 1);
-                Util.updateNum('subjects', 'accuseds', this.trial);
-                message(vm, "warning", "温馨提示：被告删除成功！");
-              } else {
-                message(vm, "error", "温馨提示：不能删除唯一的原告！");
-              }
-            }
+          for (let j = 0; j < accused.responsibles.length; j++) {
+            accused.responsibles[j].inquiry = this.getArray(len);
           }
-          break;
-        case "responsible":
-          {
-            // 添加
-            if (operation === "add") {
-              // let accusedIndex = params.accusedIndex;
-              // let originSubject = Trial().verification.participator.accuseds[0].responsibles[0];
-              // let targetSubject = this.trial.verification.participator.accuseds[accusedIndex].responsibles.push(originSubject);
-              // message(vm, "success", "温馨提示：原告法定代表人/负责人/法定代理人/指定代理人添加成功！");
-            }
-            // 删除
-            else if (operation === "remove") {
-              let accusedIndex = params.accusedIndex;
-              let responsibleIndex = params.responsibleIndex;
-              this.trial.verification.participator.accuseds[accusedIndex].responsibles.splice(responsibleIndex, 1);
-              message(vm, "warning", "温馨提示：原告法定代表人/负责人/法定代理人/指定代理人删除成功！");
-            }
+          for (let j = 0; j < accused.subjects.length; j++) {
+            accused.agents[j].inquiry = this.getArray(len);
           }
-          break;
-        case "agent":
-          {
-            // 添加
-            if (operation === "add") {
-              let accusedIndex = params.accusedIndex;
-              let originAgent = Trial().verification.participator.accuseds[0].agents[0];
-              let targetAgents = this.trial.verification.participator.accuseds[accusedIndex].agents;
-              if (targetAgents.length < 2) {
-                // 法庭询问,添加了几个问题
-                let len = this.trial.investigate.inquiry.elementquerys.length || 0;
-                if (len) {
-                  originAgent.inquiry = this.getArray(len);
-                  // console.log(originAgent.inquiry)
-                }
-                // 法庭辩论
-                let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
-                if (debateArrayLen) {
-                  originAgent.argument = this.getArgumentArray(debateArrayLen);
-                  // console.log(originAgent.inquiry)
-                }
-                targetAgents.push(originAgent);
-                message(vm, "success", "温馨提示：原告委托诉讼代理人添加成功！");
-              } else {
-                message(vm, "error", "温馨提示：不能添加更多的原告委托诉讼代理人！");
-              }
-            }
-            // 删除
-            else if (operation === "remove") {
-              let accusedIndex = params.accusedIndex;
-              let agentIndex = params.agentIndex;
-              if (agentIndex !== 0) {
-                this.trial.verification.participator.accuseds[accusedIndex].agents.splice(agentIndex, 1);
-                message(vm, "warning", "温馨提示：原告委托诉讼代理人删除成功！");
-              } else {
-                message(vm, "error", "温馨提示：不能删除唯一的原告委托诉讼代理人！");
-              }
-            }
+        }
+        // 法庭辩论，添加几轮辩论
+        let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
+        if (debateArrayLen) {
+          for (let j = 0; j < accused.subjects.length; j++) {
+            accused.subjects[j].argument = this.getArgumentArray(debateArrayLen);
           }
-          break;
-        default:
-          {
-            notify(vm, "error", "温馨提示：请传入正确的Handler操作参数！", "错误");
+          for (let j = 0; j < accused.responsibles.length; j++) {
+            accused.responsibles[j].argument = this.getArgumentArray(debateArrayLen);
           }
+          for (let j = 0; j < accused.subjects.length; j++) {
+            accused.agents[j].argument = this.getArgumentArray(debateArrayLen);
+          }
+        }
+        this.trial.verification.participator.accuseds.push(accused);
+        message(vm, "success", "温馨提示：被告添加成功！");
+      }
+      /* 删除操作 */
+      else if (operation === "remove") {
+        const accusedIndex = params.accusedIndex;
+        const accuseds = this.trial.verification.participator.accuseds;
+        if (accuseds.length > 1) {
+          accuseds.splice(accusedIndex, 1);
+          Util.updateNum('subjects', 'accuseds', this.trial);
+          message(vm, "warning", "温馨提示：被告删除成功！");
+        } else {
+          message(vm, "error", "温馨提示：不能删除唯一的被告！");
+        }
+      }
+    },
+    subjectEvent(operation, params) {
+      const vm = this;
+      /* 添加操作 */
+      if (operation === "add") {
+        let accusedIndex = params.accusedIndex;
+        let originSubject = Trial().verification.participator.accuseds[0].subjects[0];
+        originSubject["ordinal"] = Util.addNum('subjects', 'accuseds', this.trial).length + 1;
+        // 法庭询问,添加了几个问题
+        let len = this.trial.investigate.inquiry.elementquerys.length || 0;
+        if (len) {
+          originSubject.inquiry = this.getArray(len);
+        }
+        // 法庭辩论
+        let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
+        if (debateArrayLen) {
+          originSubject.argument = this.getArgumentArray(debateArrayLen);
+        }
+        let targetSubject = this.trial.verification.participator.accuseds[accusedIndex].subjects.push(originSubject);
+        message(vm, "success", "温馨提示：被告诉讼主体添加成功！");
+      }
+      /* 删除操作 */
+      else if (operation === "remove") {
+        const subjectIndex = params.subjectIndex;
+        const accusedIndex = params.accusedIndex;
+        const subjects = this.trial.verification.participator.accuseds[accusedIndex].subjects;
+        if (subjects != 0) {
+          subjects.splice(subjectIndex, 1);
+          Util.updateNum('subjects', 'accuseds', this.trial);
+          message(vm, "warning", "温馨提示：被告删除成功！");
+        } else {
+          message(vm, "error", "温馨提示：不能删除唯一的被告！");
+        }
+      }
+    },
+    responsibleEvent(operation, params) {
+      const vm = this;
+      /* 添加操作 */
+      if (operation === "add") {}
+      /* 删除操作 */
+      else if (operation === "remove") {
+        let accusedIndex = params.accusedIndex;
+        let responsibleIndex = params.responsibleIndex;
+        this.trial.verification.participator.accuseds[accusedIndex].responsibles.splice(responsibleIndex, 1);
+        message(vm, "warning", "温馨提示：被告法定代表人/负责人/法定代理人/指定代理人删除成功！");
+      }
+    },
+    agentEvent(operation, params) {
+      const vm = this;
+      /* 添加操作 */
+      if (operation === "add") {
+        let accusedIndex = params.accusedIndex;
+        let originAgent = Trial().verification.participator.accuseds[0].agents[0];
+        let targetAgents = this.trial.verification.participator.accuseds[accusedIndex].agents;
+        if (targetAgents.length < 2) {
+          // 法庭询问,添加了几个问题
+          let len = this.trial.investigate.inquiry.elementquerys.length || 0;
+          if (len) {
+            originAgent.inquiry = this.getArray(len);
+            // console.log(originAgent.inquiry)
+          }
+          // 法庭辩论
+          let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
+          if (debateArrayLen) {
+            originAgent.argument = this.getArgumentArray(debateArrayLen);
+            // console.log(originAgent.inquiry)
+          }
+          targetAgents.push(originAgent);
+          message(vm, "success", "温馨提示：被告委托诉讼代理人添加成功！");
+        } else {
+          message(vm, "error", "温馨提示：不能添加更多的被告委托诉讼代理人！");
+        }
+      }
+      /* 删除操作 */
+      else if (operation === "remove") {
+        const agentIndex = params.agentIndex;
+        const accusedIndex = params.accusedIndex;
+        const agents = this.trial.verification.participator.accuseds[accusedIndex].agents;
+        if (agents.length != 0) {
+          agents.splice(agentIndex, 1);
+          message(vm, "warning", "温馨提示：被告委托诉讼代理人删除成功！");
+        } else {
+          message(vm, "error", "温馨提示：不能删除唯一的被告委托诉讼代理人！");
+        }
       }
     },
   },
   created() {
     const params = storage.get("case");
-    this.trial.verification.participator.accuseds[0].subjects[0].name = params.defendant;
-    this.trial.verification.participator.accuseds[0].subjects[0].info = params.defendant_baseinfo;
+    this.trial.verification.participator.accuseds[0].subjects[0].name = params.accused;
+    this.trial.verification.participator.accuseds[0].subjects[0].info = params.accused_baseinfo;
   }
 };
--- sources/trial/article/produce/3-verification/thirdparty/script.js
+++ sources/trial/article/produce/3-verification/thirdparty/script.js
@@ -1,5 +1,5 @@
 import { mapMutations, mapActions, mapState, mapGetters } from "vuex";
-import { message, notify } from "../../../../../common/scripts/helper";
+import { message, notify, storage } from "../../../../../common/scripts/helper";
 import Trial from "../../script.vue.data.trial";
 import Util from "../util.js";
 
@@ -23,174 +23,149 @@ export default {
       return array
     },
     getArgumentArray(debateArrayLen) {
-      let array = [{"detail": ""}];
+      let array = [{ "detail": "" }];
       for (let i = 0; i <= debateArrayLen; i++) {
         array.push({ "detail": "" })
       }
       return array
     },
-    toCourtChange(obj) {debugger//到庭，未到庭的选择事件
-      obj.isAppear == "到庭" ? (obj.showFlag = true) : (obj.showFlag = false);
-      Util.showSetting(this.trial);
-      // vm.event.verify.verify_participant.getAbsentee(); //存储未到庭人员 （诉讼地位名：姓名）
-      // vm.event.verify.verify_participant.lagalAgentChange(obj);
-      // vm.event.verify.verify_participant.getToCourt(); //存储到庭人员 （诉讼地位名：姓名）
-    },
-    thirdpartyHandler(target, operation, params) {
+    accuserEvent(operation, params) {
       const vm = this;
-      switch (target) {
-        case "thirdparty":
-          {
-            // 添加
-            if (operation === "add") {
-              let thirdparty = Trial().verification.participator.thirdparties[0];
-              thirdparty.subjects[0]["ordinal"] = Util.addNum('subjects', 'thirdparties', this.trial).length + 1;
-
-              // 法庭询问,添加了几个问题
-              // console.log(this.trial.investigate.inquiry)
-              let len = this.trial.investigate.inquiry.elementquerys.length || 0;
-              if (len) {
-                for (let j = 0; j < thirdparty.subjects.length; j++) {
-                  thirdparty.subjects[j].inquiry = this.getArray(len);
-                }
-                for (let j = 0; j < thirdparty.responsibles.length; j++) {
-                  thirdparty.responsibles[j].inquiry = this.getArray(len);
-                }
-                for (let j = 0; j < thirdparty.subjects.length; j++) {
-                  thirdparty.agents[j].inquiry = this.getArray(len);
-                }
-                // console.log(thirdparty.subjects)
-              }
-
-                // 法庭辩论，添加几轮辩论
-              let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
-              if (debateArrayLen) {
-                for (let j = 0; j < thirdparty.subjects.length; j++) {
-                  thirdparty.subjects[j].argument = this.getArgumentArray(debateArrayLen);
-                }
-                for (let j = 0; j < thirdparty.responsibles.length; j++) {
-                  thirdparty.responsibles[j].argument = this.getArgumentArray(debateArrayLen);
-                }
-                for (let j = 0; j < thirdparty.subjects.length; j++) {
-                  thirdparty.agents[j].argument = this.getArgumentArray(debateArrayLen);
-                }
-              }
-              this.trial.verification.participator.thirdparties.push(thirdparty);
-              message(vm, "success", "温馨提示：第三人添加成功！");
-            }
-            // 删除
-            else if (operation === "remove") {
-              let accuserIndex = params.accuserIndex;
-              if (accuserIndex !== 0) {
-                let thirdparties = vm.trial.verification.participator.thirdparties;
-                thirdparties.splice(accuserIndex, 1);
-                Util.updateNum('subjects', 'thirdparties', this.trial);
-                message(vm, "warning", "温馨提示：第三人删除成功！");
-              } else {
-                message(vm, "error", "温馨提示：不能删除唯一的第三人！");
-              }
-            }
+      /* 添加操作 */
+      if (operation === "add") {
+        let thirdparty = Trial().verification.participator.thirdparties[0];
+        thirdparty.subjects[0]["ordinal"] = Util.addNum('subjects', 'thirdparties', this.trial).length + 1;
+        // 法庭询问,添加了几个问题
+        let len = this.trial.investigate.inquiry.elementquerys.length || 0;
+        if (len) {
+          for (let j = 0; j < thirdparty.subjects.length; j++) {
+            thirdparty.subjects[j].inquiry = this.getArray(len);
           }
-          break;
-        case "subject":
-          {
-            // 添加
-            if (operation === "add") {
-              let accuserIndex = params.accuserIndex;
-              let originSubject = Trial().verification.participator.thirdparties[0].subjects[0];
-              originSubject["ordinal"] = Util.addNum('subjects', 'thirdparties', this.trial).length + 1;
-              // 法庭询问,添加了几个问题
-              //  console.log(this.trial.investigate.inquiry)
-              let len = this.trial.investigate.inquiry.elementquerys.length || 0;
-              if (len) {
-                originSubject.inquiry = this.getArray(len);
-                //  console.log( originSubject.inquiry)
-              }
-              // 法庭辩论
-                let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
-                if (debateArrayLen) {
-                  originSubject.argument = this.getArgumentArray(debateArrayLen);
-                  // console.log(originAgent.inquiry)
-                }
-              let targetSubject = this.trial.verification.participator.thirdparties[accuserIndex].subjects.push(originSubject);
-              message(vm, "success", "温馨提示：第三人诉讼主体添加成功！");
-            }
-            // 删除
-            else if (operation === "remove") {
-              let accuserIndex = params.accuserIndex;
-              let subjectIndex = params.subjectIndex;
-              if (subjectIndex !== 0) {
-                this.trial.verification.participator.thirdparties[accuserIndex].subjects.splice(subjectIndex, 1);
-                Util.updateNum('subjects', 'thirdparties', this.trial);
-                message(vm, "warning", "温馨提示：第三人删除成功！");
-              } else {
-                message(vm, "error", "温馨提示：不能删除唯一的第三人！");
-              }
-            }
+          for (let j = 0; j < thirdparty.responsibles.length; j++) {
+            thirdparty.responsibles[j].inquiry = this.getArray(len);
           }
-          break;
-        case "responsible":
-          {
-            // 添加
-            if (operation === "add") {
-              // let accuserIndex = params.accuserIndex;
-              // let originSubject = Trial().verification.participator.thirdparties[0].responsibles[0];
-              // let targetSubject = this.trial.verification.participator.thirdparties[accuserIndex].responsibles.push(originSubject);
-              // message(vm, "success", "温馨提示：第三人法定代表人/负责人/法定代理人/指定代理人添加成功！");
-            }
-            // 删除
-            else if (operation === "remove") {
-              let accuserIndex = params.accuserIndex;
-              let responsibleIndex = params.responsibleIndex;
-              this.trial.verification.participator.thirdparties[accuserIndex].responsibles.splice(responsibleIndex, 1);
-              message(vm, "warning", "温馨提示：第三人法定代表人/负责人/法定代理人/指定代理人删除成功！");
-            }
+          for (let j = 0; j < thirdparty.subjects.length; j++) {
+            thirdparty.agents[j].inquiry = this.getArray(len);
           }
-          break;
-        case "agent":
-          {
-            // 添加
-            if (operation === "add") {
-              let accuserIndex = params.accuserIndex;
-              let originAgent = Trial().verification.participator.thirdparties[0].agents[0];
-              let targetAgents = this.trial.verification.participator.thirdparties[accuserIndex].agents;
-              if (targetAgents.length < 2) {
-                // 法庭询问,添加了几个问题
-                let len = this.trial.investigate.inquiry.elementquerys.length || 0;
-                if (len) {
-                  originAgent.inquiry = this.getArray(len);
-                  // console.log(originAgent.inquiry)
-                }
-                // 法庭辩论
-                let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
-                if (debateArrayLen) {
-                  originAgent.argument = this.getArgumentArray(debateArrayLen);
-                  // console.log(originAgent.inquiry)
-                }
-                targetAgents.push(originAgent);
-                message(vm, "success", "温馨提示：第三人委托诉讼代理人添加成功！");
-              } else {
-                message(vm, "error", "温馨提示：不能添加更多的第三人委托诉讼代理人！");
-              }
-            }
-            // 删除
-            else if (operation === "remove") {
-              let accuserIndex = params.accuserIndex;
-              let agentIndex = params.agentIndex;
-              if (agentIndex !== 0) {
-                this.trial.verification.participator.thirdparties[accuserIndex].agents.splice(agentIndex, 1);
-                message(vm, "warning", "温馨提示：第三人委托诉讼代理人删除成功！");
-              } else {
-                message(vm, "error", "温馨提示：不能删除唯一的第三人委托诉讼代理人！");
-              }
-            }
+        }
+        // 法庭辩论，添加几轮辩论
+        let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
+        if (debateArrayLen) {
+          for (let j = 0; j < thirdparty.subjects.length; j++) {
+            thirdparty.subjects[j].argument = this.getArgumentArray(debateArrayLen);
           }
-          break;
-        default:
-          {
-            notify(vm, "error", "温馨提示：请传入正确的Handler操作参数！", "错误");
+          for (let j = 0; j < thirdparty.responsibles.length; j++) {
+            thirdparty.responsibles[j].argument = this.getArgumentArray(debateArrayLen);
           }
+          for (let j = 0; j < thirdparty.subjects.length; j++) {
+            thirdparty.agents[j].argument = this.getArgumentArray(debateArrayLen);
+          }
+        }
+        this.trial.verification.participator.thirdparties.push(thirdparty);
+        message(vm, "success", "温馨提示：第三人添加成功！");
+      }
+      /* 删除操作 */
+      else if (operation === "remove") {
+        const accuserIndex = params.accuserIndex;
+        const thirdparties = this.trial.verification.participator.thirdparties;
+        if (thirdparties.length > 1) {
+          thirdparties.splice(accuserIndex, 1);
+          Util.updateNum('subjects', 'thirdparties', this.trial);
+          message(vm, "warning", "温馨提示：第三人删除成功！");
+        } else {
+          message(vm, "error", "温馨提示：不能删除唯一的第三人！");
+        }
+      }
+    },
+    subjectEvent(operation, params) {
+      const vm = this;
+      /* 添加操作 */
+      if (operation === "add") {
+        let accuserIndex = params.accuserIndex;
+        let originSubject = Trial().verification.participator.thirdparties[0].subjects[0];
+        originSubject["ordinal"] = Util.addNum('subjects', 'thirdparties', this.trial).length + 1;
+        // 法庭询问,添加了几个问题
+        let len = this.trial.investigate.inquiry.elementquerys.length || 0;
+        if (len) {
+          originSubject.inquiry = this.getArray(len);
+        }
+        // 法庭辩论
+        let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
+        if (debateArrayLen) {
+          originSubject.argument = this.getArgumentArray(debateArrayLen);
+        }
+        let targetSubject = this.trial.verification.participator.thirdparties[accuserIndex].subjects.push(originSubject);
+        message(vm, "success", "温馨提示：第三人诉讼主体添加成功！");
+      }
+      /* 删除操作 */
+      else if (operation === "remove") {
+        const subjectIndex = params.subjectIndex;
+        const accuserIndex = params.accuserIndex;
+        const subjects = this.trial.verification.participator.thirdparties[accuserIndex].subjects;
+        if (subjects != 0) {
+          subjects.splice(subjectIndex, 1);
+          Util.updateNum('subjects', 'thirdparties', this.trial);
+          message(vm, "warning", "温馨提示：第三人删除成功！");
+        } else {
+          message(vm, "error", "温馨提示：不能删除唯一的第三人！");
+        }
+      }
+    },
+    responsibleEvent(operation, params) {
+      const vm = this;
+      /* 添加操作 */
+      if (operation === "add") {}
+      /* 删除操作 */
+      else if (operation === "remove") {
+        let accuserIndex = params.accuserIndex;
+        let responsibleIndex = params.responsibleIndex;
+        this.trial.verification.participator.thirdparties[accuserIndex].responsibles.splice(responsibleIndex, 1);
+        message(vm, "warning", "温馨提示：第三人法定代表人/负责人/法定代理人/指定代理人删除成功！");
+      }
+    },
+    agentEvent(operation, params) {
+      const vm = this;
+      /* 添加操作 */
+      if (operation === "add") {
+        let accuserIndex = params.accuserIndex;
+        let originAgent = Trial().verification.participator.thirdparties[0].agents[0];
+        let targetAgents = this.trial.verification.participator.thirdparties[accuserIndex].agents;
+        if (targetAgents.length < 2) {
+          // 法庭询问,添加了几个问题
+          let len = this.trial.investigate.inquiry.elementquerys.length || 0;
+          if (len) {
+            originAgent.inquiry = this.getArray(len);
+            // console.log(originAgent.inquiry)
+          }
+          // 法庭辩论
+          let debateArrayLen = this.trial.argument.other.debateArray.length || 0;
+          if (debateArrayLen) {
+            originAgent.argument = this.getArgumentArray(debateArrayLen);
+            // console.log(originAgent.inquiry)
+          }
+          targetAgents.push(originAgent);
+          message(vm, "success", "温馨提示：第三人委托诉讼代理人添加成功！");
+        } else {
+          message(vm, "error", "温馨提示：不能添加更多的第三人委托诉讼代理人！");
+        }
+      }
+      /* 删除操作 */
+      else if (operation === "remove") {
+        const agentIndex = params.agentIndex;
+        const accuserIndex = params.accuserIndex;
+        const agents = this.trial.verification.participator.thirdparties[accuserIndex].agents;
+        if (agents.length != 0) {
+          agents.splice(agentIndex, 1);
+          message(vm, "warning", "温馨提示：第三人委托诉讼代理人删除成功！");
+        } else {
+          message(vm, "error", "温馨提示：不能删除唯一的第三人委托诉讼代理人！");
+        }
       }
     },
   },
+  created() {
+    const params = storage.get("case");
+    this.trial.verification.participator.thirdparties[0].subjects[0].name = params.thirdparty;
+    this.trial.verification.participator.thirdparties[0].subjects[0].info = params.accuser_baseinfo;
+  }
 };
