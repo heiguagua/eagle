f23565c029146cef2035953da3e07d8831ef0331
hank.zheng <zhengh@chinawiserv.com>
2017-12-08 15:38:51 +0800

    Update

---
+++ sources/trial/article/script.js
@@ -0,0 +1,16 @@
+import Http from "../../common/scripts/http";
+import { message, storage } from "../../common/scripts/helper";
+import { mapMutations, mapActions, mapState, mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState("Trial", [
+      "trial",
+      "options"
+    ]),
+  },
+  methods: {}
+};
