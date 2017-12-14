f4b63f680ac41d0339e074f670e352cf5036d55b
denghui <dengh@chinawiserv.com>
2017-12-14 20:32:20 +0800

    #49029

--- sources/trial/article/produce/1-infomation/script.js
+++ sources/trial/article/produce/1-infomation/script.js
@@ -13,27 +13,29 @@ export default {
       return (this.trial.infomation.officer[0].duty === "审判长") ? true : false;
     },
     isChiefOfficerTwo() {
-      const params =storage.get("case");
+      const params = storage.get("case");
       return (params.hearing_procedure === "simple") ? false : true;
     },
   },
   created() {
-    const params =storage.get("case");
+    const params = storage.get("case");
     this.trial.preface.organize.program = params.hearing_procedure;
-    this.trial.infomation.code =params.case_no;
-    this.trial.infomation.reason =params.case_brief;
-    this.trial.infomation.location.name =params.court_name;
-    if(params.hearing_procedure === "simple"){
-      this.trial.infomation.officer[0].duty ="审判员";
+    this.trial.infomation.code = params.case_no;
+    this.trial.infomation.reason = params.case_brief;
+    this.trial.infomation.location.name = params.court_name;
+    if (params.hearing_procedure === "simple") {
+      this.trial.infomation.officer[0].duty = "审判员";
     }
   },
   methods: {
     isPublic() {
-       if(this.trial.infomation.isPublic === "不公开") {
-          this.trial.infomation.audience = 0;
-       } 
+      if (this.trial.infomation.isPublic === "不公开") {
+        this.trial.infomation.audience = 0;
+      }
     },
-    setOficerFormat: function (index) {
+    setOficerFormat: function(index) {
+      const vm = this;
+      var regx = /^[\u4E00-\u9FA5A-Za-z\s\?:·\u4E00-\u9FA5\.]+$/;
       if (this.trial.infomation.officer[index].name.length === 2) {
         let value = this.trial.infomation.officer[index].name.replace(/\s+/g, '');
         let nameArr = [];
@@ -41,14 +43,34 @@ export default {
         value.substr(1, 2) ? nameArr[1] = value.substr(1, 2) : 0;
         this.trial.infomation.officer[index].name = nameArr.join('  ');
       }
+      if (!this.trial.infomation.officer[index].name) {
+        // layer.msg('姓名不能为空')
+      } else if (this.trial.infomation.officer[index].name.length >= 50) {
+        message(vm, "error", '字符过长');
+        vm.article.store.data.info.judge[name] = "";
+      } else if (!regx.test(this.trial.infomation.officer[index].name)) {
+        message(vm, "error", '姓名只能为汉字或英文');
+        this.trial.infomation.officer[index].name = "";
+      }
     },
-    setClerkFormat: function () {
+    setClerkFormat: function() {
+      const vm = this;
+      var regx = /^[\u4E00-\u9FA5A-Za-z\s\?:·\u4E00-\u9FA5\.]+$/;
       if (this.trial.infomation.clerk.length === 2) {
         let value = this.trial.infomation.clerk.replace(/\s+/g, '');
         let nameArr = [];
         nameArr[0] = value.substr(0, 1);
         value.substr(1, 2) ? nameArr[1] = value.substr(1, 2) : 0;
         this.trial.infomation.clerk = nameArr.join('  ');
+      }
+      if (!this.trial.infomation.clerk) {
+        // layer.msg('姓名不能为空')
+      } else if (this.trial.infomation.clerk.length >= 50) {
+        message(vm, "error", '字符过长');
+        this.trial.infomation.clerk = "";
+      } else if (!regx.test(this.trial.infomation.clerk)) {
+        message(vm, "error", '姓名只能为汉字或英文');
+        this.trial.infomation.clerk = "";
       }
     }
   },
