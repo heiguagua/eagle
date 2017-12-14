3182dc8f0c21b571bbabe64323cc7b3ffe32cb1c
dengtl <dengtl@chinawiserv.com>
2017-12-14 17:10:45 +0800

    preview/template

--- sources/trial/article/preview/template/script.js
+++ sources/trial/article/preview/template/script.js
@@ -7,9 +7,32 @@ export default {
     return {
       case: {},
       trial: {},
+      commonStyle: {
+        lineHeight: "30pt",
+        margin: "0 0.917cm",
+        fontFamily: "仿宋",
+        fontSize: "16pt",
+        textIndent: "30pt",
+        textAlign: "justify",
+        textJustify: "inter-ideograph"
+      },
+      center :{
+        textAlign: "center",
+      },
+      right: {
+        textAlign: "right",
+      },
+      pMargin: {
+        margin: "0"
+      }
     };
   },
-  computed: {},
+  computed: {
+    isChiefOfficer() {
+      return (this.trial.infomation.officer[0].duty === "审判长") ? true : false;
+    },
+    
+  },
   created() {
     this.case = storage.get("case");
     this.trial = storage.get("trial");
