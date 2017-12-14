609006117a12b61f8e56a0319c1132f34373f882
denghui <dengh@chinawiserv.com>
2017-12-14 21:12:52 +0800

    fix bug

--- sources/trial/article/produce/5-investigate/1-opinion/script.js
+++ sources/trial/article/produce/5-investigate/1-opinion/script.js
@@ -10,5 +10,15 @@ export default {
       "options"
     ]),
   },
-  methods: {},
+  methods: {
+    seeDetail(parentIndex,index,subject) {
+      if(subject === "subject") {
+        this.trial.verification.participator.accusers[parentIndex].subjects[index].opinion.detail = "详见民事诉讼状。";
+      }else if(subject === "responsible") {
+        this.trial.verification.participator.accusers[parentIndex].responsibles[index].opinion.detail = "详见民事诉讼状。";
+      }else if(subject === "agents") {
+        this.trial.verification.participator.accusers[parentIndex].agents[index].opinion.detail = "详见民事诉讼状。";
+      }
+    }
+  },
 };
