866fbf873d125bada5f46a19400e900cfaefbd51
dengtl <dengtl@chinawiserv.com>
2017-12-19 20:33:03 +0800

    缺席审理导航栏

--- sources/trial/navbar/script.js
+++ sources/trial/navbar/script.js
@@ -1,4 +1,7 @@
 import { mapMutations, mapActions, mapState, mapGetters } from "vuex";
+// Store
+import trial from "../article/produce/script.vue.data.trial.js";
+import options from "../article/produce/script.vue.data.options.js";
 export default {
   data() {
     return {
@@ -13,7 +16,21 @@ export default {
       "options"
     ]),
   },
+  created() {
+    const vm = this;
+    vm.operation = vm.$route.query.operation;
+    // 区分新建、修改的状态，从而挂载不同的store
+    if (vm.operation === "create") {
+      this.setOptions(options);
+      this.setTrial(trial());
+    } 
+   
+  },
   methods: {
+    ...mapMutations("Trial", [
+      "setTrial",
+      "setOptions"
+    ]),
     collapse() {
       this.menu.isCollapse = !this.menu.isCollapse;
     }
