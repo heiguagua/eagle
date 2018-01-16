eccc1ebd3315c31e995ea0bef6f160a01b7da25f
denghui <dengh@chinawiserv.com>
2018-01-16 15:05:12 +0800

    template 1\2\10

--- sources/trial/article/produce/2-discipline/script.js
+++ sources/trial/article/produce/2-discipline/script.js
@@ -6,7 +6,8 @@ export default {
   },
   computed: {
     ...mapState("Trial", [
-      "trial"
+      "trial",
+      "template"
     ]),
     isChiefOfficer() {
       return (this.trial.infomation.officer[0].duty === "审判长") ? true : false;
